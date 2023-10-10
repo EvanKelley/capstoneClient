fc67025315f85d30b1b062d26f09c82d1e2ebdc8
EvanKelley <sionnach1993@gmail.com>
2023-10-10 13:08:19 -0500

    login and styles

--- dndapp-client/src/Components/Login.js
+++ dndapp-client/src/Components/Login.js
@@ -1,8 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 
 const Login = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (event) => {
+        // event.preventDefault()
+        const currentUser = { ...userInfo }
+        currentUser[event.target.name] = event.target.value
+        setUserInfo(currentUser)
+        
+    }
+    console.log(userInfo)
+
+    const loginUser = (event) => {
+        event.preventDefault()
+        console.log("Log In Function")
+        axios.post("http://localhost:3000/auth/login", {
+            email: userInfo.email,
+            password: userInfo.password
+        })
+        .then(function (response) {
+            console.log(response)
+            document.cookie="loggedIn=true"     
+            // window.location.replace("/home")  
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+
+        setUserInfo({
+            email: "",
+            password: ""
+        })
+    }
+   
     return (
+        <Container maxWidth="sm">
         <div>Login Component</div>
+            <form onSubmit={loginUser}>
+                <TextField label="email" name="email" value={userInfo.email} onChange={(event)=>{handleChange(event)}} variant="outlined" />
+                <TextField label="password" name="password" value={userInfo.password} type="password" onChange={(event)=>{handleChange(event)}} variant="outlined" />
+                <Button variant="outlined" type="submit">Log In</Button>
+            </form>
+        </Container>
     )
 }
 
--- dndapp-client/src/Router.js
+++ dndapp-client/src/Router.js
@@ -3,12 +3,14 @@ import{ Route, Routes, Navigate } from "react-router"
 
 import Signup from "./Components/Signup"
 import Login from "./Components/Login"
+import Home from "./Components/Home"
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
         </Routes>
     ) 
 }
